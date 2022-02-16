4f14ccedaa43098f6448e8a4fa57bd4df8dde939
raghav2005 <raghavawasthi2005@gmail.com>
2022-02-16 23:13:49 +0400

    added all imports to initial_approach.py

---
+++ initial_approach.py
@@ -0,0 +1,8 @@
+import spacy
+from spacy import displacy
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import stopwords
+
