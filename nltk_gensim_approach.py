b70a96fdbb9fcc3b51deb2a5267de8a8f2b0aebc
raghav2005 <raghavawasthi2005@gmail.com>
2022-02-17 15:52:36 +0400

    finished creating bag of words for nltk_gensim_approach.py

--- nltk_gensim_approach.py
+++ nltk_gensim_approach.py
@@ -63,9 +63,11 @@ words = []
 for word in given_text.split(' '):
     words.append(word)
 
-print("Original document:", words)
-print("\n\nTokenized and lemmatized document:", preprocess(given_text), "\n\n")
+# preprocessing
+print('Original document:', words)
+print('\n\nTokenized and lemmatized document:', preprocess(given_text), '\n\n')
 
+# training
 newsgroups_train = sklearn.datasets.fetch_20newsgroups(subset = 'train', shuffle = True)
 newsgroups_test = sklearn.datasets.fetch_20newsgroups(subset = 'test', shuffle = True)
 
@@ -73,4 +75,29 @@ processed_docs = []
 
 for doc in newsgroups_train.data:
     processed_docs.append(preprocess(doc))
-print(processed_docs[ : 2])
+
+print(processed_docs[ : 2], '\n\n')
+
+# bag of words
+dictionary = gensim.corpora.Dictionary(processed_docs)
+
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+
+# remove very rare and very common
+dictionary.filter_extremes(no_below = 15, no_above = 0.1, keep_n = 100000)
+# create bag of words model for each doc
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+
+# preview bag of words for sample preprocessed document
+document_num = 20
+bow_doc_x = bow_corpus[document_num]
+
+for i in range(len(bow_doc_x)):
+    print("Word {} (\"{}\") appears {} time.".format(bow_doc_x[i][0], dictionary[bow_doc_x[i][0]], bow_doc_x[i][1]))
+
+
